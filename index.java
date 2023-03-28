0d39ad3f022e0a67f7a35e632f9cf901b064a6d8
Thilina <thilina.kasun@revocaresolutions.com>
2023-03-28 16:04:38 +0530

    I eddited java file to merge master branch

--- index.java
+++ index.java
@@ -3,5 +3,6 @@
 public class index{
     public static void main(String[] args) {
         System.out.println("My first Git Project");
+        System.out.println("Edited this to merge master branch");
     }
 }
