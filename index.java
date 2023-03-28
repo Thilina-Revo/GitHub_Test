597a8ea7adbb0a5d76d167d892835f8a21b753f7
Thilina <thilina.kasun@revocaresolutions.com>
2023-03-28 16:43:31 +0530

    Direct commit to master branch

--- index.java
+++ index.java
@@ -4,5 +4,6 @@ public class index{
     public static void main(String[] args) {
         System.out.println("My first Git Project");
         System.out.println("Edited this to merge master branch");
+        System.out.println("I eddited this directly commit to master branch");
     }
 }
