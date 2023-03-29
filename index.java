7413c555fb73337048ed96c4dca07759c36bc23e
Thilina Kasun <128912174+Thilina-Revo@users.noreply.github.com>
2023-03-29 17:16:40 +0530

    Test3 git - change the main file and comment a one line to test again (#5)
    * Try to test again -Git and github
    
    * Change the bmain file and add a comment
    
    ---------
    
    Co-authored-by: Hashini <kaushalyah000@gmail.com>

--- index.java
+++ index.java
@@ -6,10 +6,17 @@ public class index{
         System.out.println("Edited this to merge master branch");
         System.out.println("I eddited this directly commit to master branch");
         System.out.println("The documentation for Amazon Cognito recommends using the AWS Amplify Framework Authentication Library from the AWS Amplify");
-        System.out.println("Components can have three parts: markup, styles, and script logic. All three of these work together in order to deliver a working component.");
+       // System.out.println("Components can have three parts: markup, styles, and script logic. All three of these work together in order to deliver a working component.");
 
-        System.out.println("The documentation for Amazon Cognito recommends using the AWS Amplify Framework Authentication Library from the AWS Amplify");
+        
 
+    }
+
+
+    public static void testGIT_Again() {
+
+        System.out.println("Try to test git Again");
+        
     }
 
 
