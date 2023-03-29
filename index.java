c6487844a5598ec96427f5d7bbb160fafe46eeda
Thilina <thilina.kasun@revocaresolutions.com>
2023-03-29 16:41:11 +0530

    testHashine change to make comflict

--- index.java
+++ index.java
@@ -8,7 +8,8 @@ public class index{
         System.out.println("The documentation for Amazon Cognito recommends using the AWS Amplify Framework Authentication Library from the AWS Amplify");
         System.out.println("Components can have three parts: markup, styles, and script logic. All three of these work together in order to deliver a working component.");
 
-        System.out.println("The documentation for Amazon Cognito recommends using the AWS Amplify Framework Authentication Library from the AWS Amplify");
+        System.out.println("The documentation for Amazon Cognito recommends 3344556677888");
 
+        System.out.println("The documentation for Amazon Cognito recommends using the AWS Amplify Framework Authentication Library from the AWS Amplify");
     }
 }
